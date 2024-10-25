f0abf60c9b20439dea77795a09a905ff95b58064
asherhepner12 <164114192+asherhepner12@users.noreply.github.com>
2024-10-25 01:26:06 -0500

    Added Collision/Lighting to mess hall

--- squash_the_creeps_start_1.1.0/main.gd
+++ squash_the_creeps_start_1.1.0/main.gd
@@ -3,6 +3,8 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _process(delta: float) -> void:
+	print("FPS: ", Engine.get_frames_per_second())
 
 func _on_mob_timer_timeout():
 	#Create new instance of the Mob scene
@@ -10,11 +12,14 @@ func _on_mob_timer_timeout():
 	
 	#Choose random location on SpawnPath
 	#store reference to SpawnLocation node
-	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	mob_spawn_location.progress_ratio = randf()
+	var mob_spawn_location = $Player.position
+	mob_spawn_location.x += randf_range(0,10)
+	mob_spawn_location.z += randf_range(0,10)
+	#get_node("SpawnPath/SpawnLocation")
+	#mob_spawn_location.progress_ratio = randf()
 	
 	var player_position = $Player.position
-	mob.initialize(mob_spawn_location.position, player_position)
+	mob.initialize(mob_spawn_location, player_position)
 	
 	#Span mob, add it to Main scene
 	add_child(mob)
